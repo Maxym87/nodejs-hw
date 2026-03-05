62a041082a0e26c546fe78c5e81c4c189f739e46
Maks Shkafar <m.shkafar@gmail.com>
2026-03-05 18:16:55 +0100

    feat: add Joi validation schema for note creation

--- src/models/note.js
+++ src/models/note.js
@@ -1,5 +1,6 @@
 import { Schema } from "mongoose";
 import { model } from "mongoose";
+import { TAGS } from "../constants/tags.js";
 const notesSchema = new Schema(
   {
 title: {
@@ -14,7 +15,7 @@ content: {
 },
 tag: {
   type: String,
-  enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+  enum: TAGS,
 default: 'Todo',
 },
   },
