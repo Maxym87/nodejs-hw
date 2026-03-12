405bcef65e302deaf14212de17c4df50adf64bc2
Maks Shkafar <m.shkafar@gmail.com>
2026-03-12 22:44:16 +0100

    feat(users): add user avatar upload endpoint

---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+
+import createHttpError from "http-errors";
+
+
+export const updateUserAvatar = async (req, res, next) => {
+if(!req.file) {
+  throw createHttpError(400, "No file");
+}
+
+  res.status(200).json({ url: "" });
+};
